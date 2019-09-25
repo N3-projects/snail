75ee0a4f75ece86dc8c53598cca605102a2e89d2
N3 <yemao5@126.com>
2019-09-26 01:57:25 +0800

    datasource使用druid实现

--- spring/src/main/java/n3/snail/spring/jpa/configuration/DataSourceConfiguration.java
+++ spring/src/main/java/n3/snail/spring/jpa/configuration/DataSourceConfiguration.java
@@ -1,31 +1,23 @@
 package n3.snail.spring.jpa.configuration;
 
-//import com.alibaba.druid.pool.DruidDataSource;
-//import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class DataSourceConfiguration {
 
-//    @Primary
-//    @Bean(initMethod = "init")
-//    @ConfigurationProperties("spring.datasource.druid")
-//    public DataSource dataSourceOne(){
-//        return DruidDataSourceBuilder.create().build();
-//    }
-
-//    @Bean
-//    @ConfigurationProperties("spring.datasource.druid.two")
-//    public DataSource dataSourceTwo(){
-//        return DruidDataSourceBuilder.create().build();
-//    }
+    @Primary
+    @Bean
+    @ConfigurationProperties("spring.datasource.druid.app.main")
+    public DruidDataSource appMainDataSource(){
+        DruidDataSource build = DruidDataSourceBuilder.create().build();
+        //获取连接时最大等待时间，单位毫秒。配置了maxWait之后，缺省启用公平锁（会影响性能），可手动设置非公平锁
+        build.setUseUnfairLock(true);
+        return build;
+    }
 
 }
